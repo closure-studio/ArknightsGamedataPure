632ee9e542aa505019fbfbc1aaf37dcc2eebbcd9
jackjieyyy <jackjieyyy@gmail.com>
2026-06-18 12:31:56 +0000

    update

---
+++ [uc]lua/hotfixes/DefinedFix.lua
@@ -0,0 +1,17 @@
+
+local list = 
+{
+  
+  "HotFixes/TestStubHotfixer",
+  "HotFixes/SandboxV3CatchedAnimalManagerHotfixer",
+  "HotFixes/VSandBoxFurnitureEntityHotfixer",
+  "HotFixes/ReturnConstructMaterialProcessorHolderHotfixer",
+  "HotFixes/SandboxV3BasementDetailStateHotfixer",
+  "HotFixes/SandboxV3BuildSaveManagerHotfixer",
+  "HotFixes/ConvexHullHotfixer",
+  "HotFixes/Act24sideQuestStateHotfixer",
+  "HotFixes/RebuildCharacterOnTileInRangeHotfixer",
+  "HotFixes/RebuildCharacterOnRandomTileHotfixer",
+};
+
+return list;
