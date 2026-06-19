36743fc9d0a8e63c6b788d7974659c53b8a4f18a
jackjieyyy <jackjieyyy@gmail.com>
2026-06-19 00:35:04 +0000

    Rollback to eddc3771

---
+++ [uc]lua/hotfixes/definedfix.lua
@@ -0,0 +1,14 @@
+
+local list = 
+{
+  
+  "HotFixes/TestStubHotfixer",
+  "HotFixes/SandboxV3CatchedAnimalManagerHotfixer",
+  "HotFixes/VSandBoxFurnitureEntityHotfixer",
+  "HotFixes/ReturnConstructMaterialProcessorHolderHotfixer",
+  "HotFixes/SandboxV3BasementDetailStateHotfixer",
+  "HotFixes/SandboxV3BuildSaveManagerHotfixer",
+  "HotFixes/ConvexHullHotfixer",
+};
+
+return list;
